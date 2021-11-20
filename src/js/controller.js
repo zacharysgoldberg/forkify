db772135a55e975395467541fe9a5fdfdc0d19a3
Zach-S-Goldberg <zacharysgoldberg@outlook.com>
2021-11-20 11:01:48 -0800

    Added new welcome feature

--- src/js/controller.js
+++ src/js/controller.js
@@ -131,6 +131,11 @@ const controlAddRecipe = async function (newRecipe) {
     addRecipeView.renderError(err.message);
   }
 };
+
+const newFeature = function () {
+  console.log('Welcome to the application');
+};
+
 const init = function () {
   bookmarksView.addHandlerRender(controlBookmarks);
   recipeView.addHandlerRender(controlRecipes);
@@ -139,6 +144,6 @@ const init = function () {
   searchView.addHandlerSearch(controlSearchResults);
   paginationView.addHandlerClick(controlPagination);
   addRecipeView.addHandlerUpload(controlAddRecipe);
-  console.log('Welcome!');
+  newFeature();
 };
 init();
