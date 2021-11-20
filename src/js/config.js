6ac147c490c188a159cd8f27988d06908c2ab5ed
Zach-S-Goldberg <zacharysgoldberg@outlook.com>
2021-11-20 10:52:15 -0800

    Initial commit

---
+++ src/js/config.js
@@ -0,0 +1,5 @@
+export const API_URL = 'https://forkify-api.herokuapp.com/api/v2/recipes/';
+export const TIMEOUT_SEC = 10;
+export const RES_PER_PAGE = 10;
+export const KEY = '8da03456-feae-4689-bf2a-382d21144cfd';
+export const MODAL_CLOSE_SEC = 2500;
