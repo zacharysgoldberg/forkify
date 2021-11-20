a156c77d1e7bcd4d4250ed879cd4122410c844cf
Zach-S-Goldberg <zacharysgoldberg@outlook.com>
2021-11-20 13:35:38 -0800

    updated fraction to numberToFraction (custom function)

--- src/js/views/recipeView.js
+++ src/js/views/recipeView.js
@@ -2,7 +2,7 @@ import View from './View.js';
 
 // import icons from '../img/icons.svg'; // Parcel 1
 import icons from 'url:../../img/icons.svg'; // Parcel 2
-import { Fraction } from 'fraction.js'; // rather than: new Fraction.Fraction()
+import { numberToFraction } from '../helpers.js'; // rather than: new Fraction.Fraction()
 
 class RecipeView extends View {
   _parentElement = document.querySelector('.recipe');
@@ -129,7 +129,7 @@ class RecipeView extends View {
       <use href="${icons}#icon-check"></use>
     </svg>
     <div class="recipe__quantity">${
-      ing.quantity ? new Fraction(ing.quantity).toString() : ''
+      ing.quantity ? numberToFraction(ing.quantity).toString() : ''
     }</div>
     <div class="recipe__description">
       <span class="recipe__unit">${ing.unit}</span>
